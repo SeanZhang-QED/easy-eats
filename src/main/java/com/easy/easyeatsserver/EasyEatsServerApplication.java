fd504c56e14e483f9e28a6d52e3b21bfde041e95
SeanZhang-QED <66594541+SeanZhang-QED@users.noreply.github.com>
2022-06-20 02:20:15 -0700

    first commit
    Project set up.

---
+++ src/main/java/com/easy/easyeatsserver/EasyEatsServerApplication.java
@@ -0,0 +1,13 @@
+package com.easy.easyeatsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyEatsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyEatsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easy/easyeatsserver/EasyEatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.easyeatsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyEatsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
